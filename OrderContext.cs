f91120a98043bea4a6c9c2e85199a997803ff673
julie <julielerman@gmail.com>
2018-02-11 12:07:09 -0500

    nulls (added, modified) & replace owned entity seem both to be working

--- OrderContext.cs
+++ OrderContext.cs
@@ -1,44 +1,43 @@
 using System;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace OwnedEntityDemo {
-  public class OrderContext:DbContext{
+  public class OrderContext : DbContext {
     public DbSet<SalesOrder> SalesOrders { get; set; }
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-       modelBuilder.Entity<SalesOrder>().OwnsOne(s=>s.BillingAddress);
-       modelBuilder.Entity<SalesOrder>().OwnsOne(s=>s.ShippingAddress);
-     
+    protected override void OnModelCreating (ModelBuilder modelBuilder) {
+      modelBuilder.Entity<SalesOrder> ()
+        .OwnsOne (s => s.BillingAddress)
+        .UsePropertyAccessMode (PropertyAccessMode.FieldDuringConstruction);
+      modelBuilder.Entity<SalesOrder> ()
+        .OwnsOne (s => s.ShippingAddress)
+        .UsePropertyAccessMode (PropertyAccessMode.FieldDuringConstruction);
     }
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
-       optionsBuilder.UseSqlite("Data Source=SalesOrder.db");
-     }
-    
-    public override int SaveChanges()
-    {
-      foreach (var entry in ChangeTracker.Entries().Where(e => e.Entity is SalesOrder &&
-       (e.State == EntityState.Added||e.State==EntityState.Modified)))
-           {
-        if (entry.Entity is SalesOrder order)
-        {
-          if (entry.Reference("ShippingAddress").CurrentValue == null)
-          {
-            entry.Reference("ShippingAddress").CurrentValue = PostalAddress.Empty();
+    protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder) {
+      optionsBuilder.UseSqlite ("Data Source=SalesOrder.db");
+    }
+
+    public override int SaveChanges () {
+      foreach (var entry in ChangeTracker.Entries ().Where (e => e.Entity is SalesOrder &&
+          (e.State == EntityState.Added || e.State == EntityState.Modified))) {
+        if (entry.Entity is SalesOrder order) {
+          if (entry.Reference ("ShippingAddress").CurrentValue == null) {
+            entry.Reference ("ShippingAddress").CurrentValue = PostalAddress.Empty ();
           }
-           if (entry.Reference("BillingAddress").CurrentValue == null)
-          {
-            entry.Reference("BillingAddress").CurrentValue = PostalAddress.Empty();
+          if (entry.Reference ("BillingAddress").CurrentValue == null) {
+            entry.Reference ("BillingAddress").CurrentValue = PostalAddress.Empty ();
           }
+          entry.Reference ("ShippingAddress").TargetEntry.State = entry.State;
+          entry.Reference ("BillingAddress").TargetEntry.State = entry.State;
         }
       }
-      return base.SaveChanges();
+      return base.SaveChanges ();
     }
 
   }
- 
+
 }
--- Program.cs
+++ Program.cs
@@ -1,38 +1,61 @@
 ﻿using System;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
-namespace OwnedEntityDemo
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        using (var context=new OrderContext())
-        {
-            context.Database.EnsureDeleted();
-            context.Database.EnsureCreated();
-        }
-            InsertNewOrder();
-            RetrieveOrder();
+namespace OwnedEntityDemo {
+    class Program {
+        static void Main (string[] args) {
+            using (var context = new OrderContext ()) {
+                context.Database.EnsureDeleted ();
+                context.Database.EnsureCreated ();
+            }
+            InsertNewOrder ();
+            RetrieveUpdateAndSaveOrder ();
         }
 
-    private static void InsertNewOrder()
-    {
-      var order=new SalesOrder(DateTime.Today, 100.00M);
-      order.ShippingAddress=PostalAddress.Create("One Main","Burlington", "VT", "05000");
-      //order.BillingAddress=new PostalAddress("Two Main","Burlington", "VT", "05000");
-      using(var context=new OrderContext()){
-          context.SalesOrders.Add(order);
-          context.SaveChanges();
-      }
+        private static void RetrieveUpdateAndSaveOrder () {
+            var order = RetrieveOrder ();
+            Console.WriteLine ("Before_____");
+            Console.WriteLine ("Ship to: " + order.ShippingAddress?.Street);
+            Console.WriteLine ("Bill to: " + order.BillingAddress?.Street);
+            //updating an existing owned entity, workaround
+            order.SetBillingAddress (PostalAddress.Create ("Broadway", "NYC", "NY", "10001"));
+            using (var context = new OrderContext ()) {
+              context.SalesOrders.Update(order);
+              context.SaveChanges ();
+            }
+            var storedOrder = RetrieveOrder ();
+            Console.WriteLine ("After_______");
+            Console.WriteLine ("Ship to: " + storedOrder.ShippingAddress?.Street);
+            Console.WriteLine ("Bill to: " + storedOrder.BillingAddress?.Street);
+
+        }
+
+        private static void InsertNewOrder () {
+            var order = new SalesOrder (DateTime.Today, 100.00M);
+            //order.SetShippingAddress (PostalAddress.Create ("One Main", "Burlington", "VT", "05000"));
+            order.SetBillingAddress (PostalAddress.Create ("Two Main", "Burlington", "VT", "05000"));
+            //order.CopyShippingAddressToBillingAddress();
+            using (var context = new OrderContext ()) {
+                context.SalesOrders.Add (order);
+                context.SaveChanges ();
+            }
+
+        }
+
+        private static SalesOrder RetrieveOrder () {
+            using (var context = new OrderContext ()) {
+                var order = context.SalesOrders.FirstOrDefault ();
+                FixOptionalValueObjects (order);
+                return order;
+            }
+        }
+
+        private static void FixOptionalValueObjects (SalesOrder order) {
+            if (order.ShippingAddress.IsEmpty ()) { order.SetShippingAddress (null); }
+            if (order.BillingAddress.IsEmpty ()) { order.SetBillingAddress (null); }
+
+        }
 
     }
-    private static void RetrieveOrder()
-    {
-       using(var context=new OrderContext()){
-          var order=context.SalesOrders.FirstOrDefault();
-        
-      }  
-    }
-  }
 }
